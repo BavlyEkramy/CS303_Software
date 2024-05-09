0173631853b76e6cc010076bbaec40a7c9b73644
bavly <bavlyakramy@gmail.com>
2024-05-09 15:24:08 +0300

    solve some issues

--- firebase/Courses.ts
+++ firebase/Courses.ts
@@ -1,110 +1,124 @@
 import { useState } from "react";
-import { db , auth } from "./Config";
+import { db, auth } from "./Config";
 import {
-     getFirestore,
-     collection,
-     getDocs,
-     setDoc,
-     onSnapshot,
-     deleteDoc,
-     doc,
-     query,
-     where,
-     getDoc,
-     updateDoc,
-     addDoc,
-     Timestamp,
-     serverTimestamp,
-     orderBy,
-   } from "firebase/firestore";
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  getDoc,
+  updateDoc,
+  addDoc,
+  Timestamp,
+  serverTimestamp,
+  orderBy,
+} from "firebase/firestore";
 
-   const colCourse = collection(db, "Courses" );
+const colCourse = collection(db, "Courses");
 
+onSnapshot(colCourse, (snapshot) => {
+  let Courses = [];
+  snapshot.docs.forEach((use) => {
+    Courses.push({ ...use.data(), id: use.id });
+  });
+  console.log("All of courses ", Courses);
+});
 
 //////////-------------------    take object to add new course  -----------/////////
- async function AddCourse(course , admin) {
-   let res = await addDoc(colCourse, { 
-     ...course,
-     admin : admin,
-   });
-   return res;
- }
+async function AddCourse(course, admin) {
+  let res = await addDoc(colCourse, {
+    ...course,
+    admin: admin,
+  });
+  return res;
+}
 ////////////-------------------    get current course   -----------/////////
-async function GetCourse() {
-     const course = (await getDocs(colCourse)).docs;
-     const c = [];
-     course.forEach( (doc) => {
-       c.push({ ...doc.data(), id: doc.id });
-     });
-     return c;
+async function GetCourses() {
+  const course = (await getDocs(colCourse)).docs;
+  const c = [];
+  course.forEach((doc) => {
+    c.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(c);
+  return c;
 }
 
+////////-------------------    Delete course   -----------////
+async function DelCourse(course) {
+  const docRef = doc(colCourse, course.id);
+  await deleteDoc(docRef);
+}
 
-
- ////////-------------------    Delete course   -----------////
-  async function DelCourse(course) {
-    const docRef = doc(colCourse, course.id);
-    await deleteDoc(docRef); 
-  }
-
-
-
-   ////////-------------------    update course   -----------////
-   async function updateCourse(course ) {
-     const docRef = doc(colCourse, course.id);
-     await updateDoc(docRef, course);
-   }
+////////-------------------    update course take object for course  -----------////
+async function updateCourse(course) {
+  const docRef = doc(colCourse, course.id);
+  await updateDoc(docRef, course);
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    /////----------------- add chapter for course ------------------/////
-   async function addChapter(course , c) {
-     const coll = collection(db, `Courses/ ${course.id}/ chapters`);
-     const t = []
-     c.videos.forEach( (video) => {
-          
-     })
-     let res = await addDoc(coll, {
-       ...c,
-       isComleted : false,
-       isOpen : false,
-     });
-     return res;
-   }
-
-
-   //     -------------------    update chapter   -----------////
-   async function updateChapter(idCourse , c) {
-     const coll = collection(db, `Courses/ ${idCourse}/ chapters`);
-     const docRef = doc(coll, c.id);
-      await updateDoc(docRef, c);
+/////------------- add one chapter for course take course Id and object for chapter  -------------/////
+async function addChapter(idCourse, ch) {
+  const coll = collection(db, `Courses/${idCourse}/chapters`);
+  ch.videos.forEach((video, index) => {
+    ch.videos[index] = { ...video, isComleted: false, isOpen: false };
+  });
+  let res = await addDoc(coll, {
+    ...ch,
+    isComleted: false,
+    isOpen: false,
+  });
+  return res;
 }
 
+//     -------------------    update chapter   -----------////
+async function updateChapter(idCourse, c) {
+  const coll = collection(db, `Courses/${idCourse}/chapters`);
+  const docRef = doc(coll, c.id);
+  await updateDoc(docRef, c);
+}
+
+//     -------------------    get chapters for course take course id   -----------////
 async function getChapters(idCourse) {
-  const coll = collection(db, `Courses/ ${idCourse}/ chapters`);
-  const querySnapshot = await getDocs(coll);
+  console.log(idCourse);
+
+  const coll = collection(db, `Courses/${idCourse}/chapters`);
+  const querySnapshot = (await getDocs(coll)).docs;
   let chapters = [];
   querySnapshot.forEach((doc) => {
     chapters.push({ ...doc.data(), id: doc.id });
   });
+  console.log(chapters);
   return chapters;
 }
 
-async function deleteChapter( idCourse , idChapter ) {
-      const coll = collection(db, `Courses/ ${idCourse}/ chapters`);
-      const docRef = doc(coll, idChapter);
-      await deleteDoc(docRef);
+////////-------------------    Delete chapter take id of course and id of chapter   -----------////
+async function deleteChapter(idCourse, idChapter) {
+  const coll = collection(db, `Courses/${idCourse}/chapters`);
+  const docRef = doc(coll, idChapter);
+  await deleteDoc(docRef);
 }
-
 
 function sup() {
-const yyyy = onSnapshot(colCourse, (snapshot) => {
-     let user = [];
-     snapshot.docs.forEach((use) => {
-       user.push({ ...use.data(), id: use.id });
-     });
-    return( user);
-   });
+  const yyyy = onSnapshot(colCourse, (snapshot) => {
+    let user = [];
+    snapshot.docs.forEach((use) => {
+      user.push({ ...use.data(), id: use.id });
+    });
+    return user;
+  });
 }
-  export { AddCourse, DelCourse , updateCourse , addChapter , updateChapter , getChapters , deleteChapter , sup , GetCourse  };
-
+export {
+  AddCourse,
+  DelCourse,
+  updateCourse,
+  addChapter,
+  updateChapter,
+  getChapters,
+  deleteChapter,
+  sup,
+  GetCourses,
+};
