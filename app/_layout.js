5ec555052b479a21686fc391801deea8846860d2
IslamShaker <IslamElzantot@gmail.com>
2024-05-11 22:55:55 +0300

    Merge Welcome Screens With Home And Profile Page

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 import React from "react";
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 // Import your global CSS file
 import "../global.css";
@@ -8,43 +8,11 @@ import "../global.css";
 export default function _layout() {
   return (
     <>
-      {/* <Slot /> */}
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerTitle: "Welcome",
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="users/[id]"
-          options={{
-            headerTitle: "User",
-          }}
-        />
-        <Stack.Screen
-          name="account/Register"
-          options={{
-            headerTitle: "Register",
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="account/Login"
-          options={{
-            headerTitle: "Login",
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="account/ForgetPassword"
-          options={{
-            headerTitle: "ForgetPassword",
-            headerShown: false,
-          }}
-        />
-      </Stack>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </>
   );
 }
